7d2743babd387ae7ddbb975f4ffceecab6044995
Miraj Desai <mirajdesai01@gmail.com>
2023-03-04 17:37:09 -0500

    get rid of warnings

--- src/tuiter/navigation-sidebar/index.js
+++ src/tuiter/navigation-sidebar/index.js
@@ -13,45 +13,45 @@ const NavigationSidebar = (
 
   return (
     <div className="list-group">
-      <a className="list-group-item">
+      <a href="#!" className="list-group-item">
         <FontAwesomeIcon icon="fa-brands fa-twitter" fixedWidth />
       </a>
-      <a className={`list-group-item
+      <a href="#!" className={`list-group-item
                     ${active === 'home' ? 'active' : ''}`}>
         <FontAwesomeIcon icon="fa-solid fa-house" fixedWidth />
         <span className="d-none d-xl-inline">Home</span>
       </a>
-      <a className={`list-group-item
+      <a href="#!" className={`list-group-item
                     ${active === 'explore' ? 'active' : ''}`}>
         <FontAwesomeIcon icon="fa-solid fa-hashtag" fixedWidth />
         <span className="d-none d-xl-inline">Explore</span>
       </a>
-      <a className={`list-group-item
+      <a href="#!" className={`list-group-item
                     ${active === 'notifications' ? 'active' : ''}`}>
         <FontAwesomeIcon icon="fa-solid fa-bell" fixedWidth />
         <span className="d-none d-xl-inline">Notifications</span>
       </a>
-      <a className={`list-group-item
+      <a href="#!" className={`list-group-item
                     ${active === 'messages' ? 'active' : ''}`}>
         <FontAwesomeIcon icon="fa-solid fa-envelope" fixedWidth />
         <span className="d-none d-xl-inline">Messages</span>
       </a>
-      <a className={`list-group-item
+      <a href="#!" className={`list-group-item
                     ${active === 'bookmarks' ? 'active' : ''}`}>
         <FontAwesomeIcon icon="fa-solid fa-bookmark" fixedWidth />
         <span className="d-none d-xl-inline">Bookmarks</span>
       </a>
-      <a className={`list-group-item
+      <a href="#!" className={`list-group-item
                     ${active === 'lists' ? 'active' : ''}`}>
         <FontAwesomeIcon icon="fa-solid fa-list-ul" fixedWidth />
         <span className="d-none d-xl-inline">Lists</span>
       </a>
-      <a className={`list-group-item
+      <a href="#!" className={`list-group-item
                     ${active === 'profile' ? 'active' : ''}`}>
         <FontAwesomeIcon icon="fa-solid fa-user" fixedWidth />
         <span className="d-none d-xl-inline">Profile</span>
       </a>
-      <a className={`list-group-item
+      <a href="#!" className={`list-group-item
                     ${active === 'more' ? 'active' : ''}`}>
         <FontAwesomeIcon icon="fa-solid fa-ellipsis-h" fixedWidth />
         <span className="d-none d-xl-inline">More</span>
